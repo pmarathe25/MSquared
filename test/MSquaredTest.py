edcdb9ef171f3b9468e6d3abb85befd3f262f972
pmarathe25 <pmarathe25@gmail.com>
2018-05-07 23:08:45 -0700

    Updates unittest setup

--- test/MSquaredTest.py
+++ test/MSquaredTest.py
@@ -4,21 +4,20 @@ import msquared as m2
 
 class MSquaredTest(unittest.TestCase):
     def setUp(self):
-        pass
+        self.make_gen = m2.MGen()
 
     def test_generate_project_makefile(self):
-        make_gen = m2.MGen()
         # Make sure we compile/link with optimizations enabled.
-        make_gen.add_flags("-O3 -flto -march=native")
+        self.make_gen.add_flags("-O3 -flto -march=native")
         # Also compile with c++-17.
-        make_gen.add_cflags("-std=c++17")
+        self.make_gen.add_cflags("-std=c++17")
         # Create a new target with the path to desired executable and use the source file(s) involved.
-        make_gen.add_executable("test/test", "test/test.cpp")
+        self.make_gen.add_executable("test/test", "test/test.cpp")
         # Create a clean target passing in extra files to be deleted
         # (by default, only the build directory is emptied)
-        make_gen.add_clean("test/test")
+        self.make_gen.add_clean("test/test")
         # Generate the makefile
-        make_gen.generate("./makefile")
+        self.make_gen.generate("./makefile")
 
     def tearDown(self):
         pass
