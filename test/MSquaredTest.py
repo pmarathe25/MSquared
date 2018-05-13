4eaad5cd7f748788569d370fd988af7d59906fca
pmarathe25 <pmarathe25@gmail.com>
2018-05-12 19:06:07 -0700

    Removes unnecessary intermediate test directory

---
+++ test/MSquaredTest.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import unittest
+import msquared as m2
+
+class MSquaredTest(unittest.TestCase):
+    def setUp(self):
+        # Add this directory to the project.
+        self.make_gen = m2.MGen("./")
+
+    def test_generate_project_makefile(self):
+        # Make sure we compile/link with optimizations enabled.
+        self.make_gen.add_flags("-O3 -flto -march=native")
+        # Also compile with c++-17.
+        self.make_gen.add_cflags("-std=c++17")
+        # Create a new target with the path to desired executable and use the source file(s) involved.
+        # Should be removed during clean!
+        self.make_gen.add_executable("test/test", ["test/test.cpp", "src/source1.cpp", "src/source2.cpp"], clean=True)
+        # Add a custom target that will run the test executable.
+        self.make_gen.add_custom_target("test", dependencies="test/test", command="test/test")
+        # Write the makefile
+        self.make_gen.write("makefile")
+        # DEBUG:
+        print(self.make_gen.generate())
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/test/test.cpp
@@ -0,0 +1,10 @@
+// External - build system shouldn't care about these.
+#include <vector>
+#include <string>
+// Internal dependencies
+#include "header1.hpp"
+#include "header2.hpp"
+
+int main() {
+    return 0;
+}
