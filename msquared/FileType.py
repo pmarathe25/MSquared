d1a1d2689eadc493313a65209f8e08f007f9c740
pmarathe25 <pmarathe25@gmail.com>
2018-05-28 19:45:30 -0700

    Adds FileType.py

---
+++ msquared/FileType.py
@@ -0,0 +1,18 @@
+from enum import IntEnum
+import os
+
+class FileType(IntEnum):
+    SOURCE = 0
+    OBJECT = 1
+    SHARED_OBJECT = 2
+    EXECUTABLE = 3
+
+def file_type(filename: str) -> FileType:
+    if os.path.splitext(filename)[1] == ".so":
+        return FileType.SHARED_OBJECT
+    elif os.path.splitext(filename)[1] == ".o":
+        return FileType.OBJECT
+    elif '.c' in os.path.splitext(filename)[1]:
+        # Match most C/C++ extensions.
+        return FileType.SOURCE
+    return FileType.EXECUTABLE
